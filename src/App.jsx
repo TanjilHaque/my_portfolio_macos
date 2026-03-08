4a53601f12c82ed90eb15bd667747252d5423340
TanjilHaque <thisismydesign9@gmail.com>
2026-03-08 12:43:16 +0600

    Welcome Page Complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import Navbar from "#components/Navbar";
-import React from "react";
+import Welcome from "#components/Welcome";
 
 const App = () => {
   return (
     <main>
       <Navbar />
+      <Welcome />
     </main>
   );
 };
